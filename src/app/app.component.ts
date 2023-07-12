caef36c5fd3f1c0d5ad3d0cf2704969a15e51a75
samndambuki <sam.ndambuki08@gmail.com>
2023-07-12 10:36:05 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>
+    <h1>{{ pageTitle }}</h1>
+    <div>My first Component</div>
+  </div>`,
 })
 export class AppComponent {
-  title = 'angularProj';
+  pageTitle: string = 'Acme Product Management';
 }
