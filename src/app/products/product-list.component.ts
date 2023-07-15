ad1d00fa8463f5a31c77f053f7e811690c7a5e51
samndambuki <sam.ndambuki08@gmail.com>
2023-07-15 08:26:47 +0300

    Built in directives

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,12 +1,32 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector:'app-products',
-    templateUrl:'./product-list.component.html'
+  selector: 'app-products',
+  templateUrl: './product-list.component.html',
 })
+export class ProductListComponent {
+  pageTitle: string = 'Product List';
 
-export class ProductListComponent{
-    pageTitle:string = "Product List"
-    
-    
+  products: any[] = [
+    {
+      productId: 2,
+      productName: 'Garden-dart',
+      productCode: 'GDN-0023',
+      releaseDate: 'MARCH 18,2021',
+      description: '15 gallon capacity',
+      price: 32.99,
+      starRating: 4.2,
+      imageUrl: 'assets/images/garden_cart.png',
+    },
+    {
+      productId: 5,
+      productName: 'Hammer',
+      productCode: 'TBX-0048',
+      releaseDate: 'May 21 ,2021',
+      description: 'Curved claw steel hammer',
+      price: 8.9,
+      starRating: 4.8,
+      imageUrl: 'assets/images/hammer.png',
+    },
+  ];
 }
