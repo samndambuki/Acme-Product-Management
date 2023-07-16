360d61538f3f6def67ddb67c9d6f023c4b031343
samndambuki <sam.ndambuki08@gmail.com>
2023-07-16 21:38:18 +0300

    activating a route with code

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Iproduct } from './products';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,6 +9,13 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
+  //display detail for one product
+  //declare a property
+
+  //property is undeined until data is retrieved
+  //its of type IProduct
+  product:Iproduct | undefined;
+
 
   //this component needs to read the route parameter
   //so as to know which products details to display
@@ -15,13 +23,16 @@ export class ProductDetailComponent implements OnInit {
   //Activated Route Service provides the state of the current route, including route parameters
 
   //set activated route as a dependency by defining it as a parameter to the constructor function
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,private router:Router) {}
 
   //called when the component is initialized
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.pageTitle += ` : ${id}`
+    this.pageTitle += ` : ${id}`; 
   }
 
-
+  onBack():void{
+    this.router.navigate(['/products'])
+  }
+  
 }
