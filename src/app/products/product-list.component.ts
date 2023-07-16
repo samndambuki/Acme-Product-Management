3e9aefc9710dd7e58708c8f3524dfd09f4b84a99
samndambuki <sam.ndambuki08@gmail.com>
2023-07-16 09:26:05 +0300

    ng OnDestroy worked

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Iproduct } from './products';
 import { ProductService } from './product.service';
-import { Observable, filter, map, range } from 'rxjs';
+import { Observable, Subscription, filter, map, range } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit,OnDestroy {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
@@ -16,6 +16,13 @@ export class ProductListComponent implements OnInit {
   //images are not displayed when page is loaded
   showImage: boolean = false;
   // listFilter: string = 'cart';
+
+  errorMessage: string = '';
+
+  //typescript strict checking
+  //cant declare a variable without assigning a default value
+  //! - hanlde assignment some time later
+  sub!: Subscription;
 
   //_ - to denote it as private
   //backing variablr
@@ -37,7 +44,7 @@ export class ProductListComponent implements OnInit {
 
   //accessor - private, public, protected
 
-  constructor(private productService:ProductService){}
+  constructor(private productService: ProductService) {}
 
   performFilter(filterBy: string): Iproduct[] {
     filterBy = filterBy.toLocaleLowerCase();
@@ -55,17 +62,32 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    //call subscribe method
+    this.sub = this.productService.getProducts().subscribe({
+      //pass in observer object
+      //specify what to do when observable emits next value
+      //since this is a http request, it only emits one time
+      //array of products is returnes in the response
+      //assign ou local products variable - this.products to the returned array of products
+      next: (products) => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: (err) => (this.errorMessage = err),
+    });
+
     console.log('In OnInit');
     // this.listFilter = '';
   }
 
-
-  onRatingClicked(message:string):void{
-    this.pageTitle = 'Product List : ' + message
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
-   
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List : ' + message;
+  }
+
   //normal function to capitalize
   // capitalizeName(product:Iproduct) :string{
   //   return product.productName.toUpperCase();
@@ -79,25 +101,21 @@ export class ProductListComponent implements OnInit {
   //   console.log(product.productName);
   //   return product.productName.toUpperCase();
   // }
-
 }
 
-  //number - geeric argument
-  // const source$:Observable<number> = range(0,10);
-  // source$.pipe(
-    //two operators
-    //transform each emitted item
-  //   map(x=>x*3),
-  //x is the item omitted from prior operator
-  //   filter(x=>x%2 === 0)
-  //start the observable using subscribe method
-  //observable omits a range of numbers
-  // ).subscribe(x=>console.log(x));
-
+//number - geeric argument
+// const source$:Observable<number> = range(0,10);
+// source$.pipe(
+//two operators
+//transform each emitted item
+//   map(x=>x*3),
+//x is the item omitted from prior operator
+//   filter(x=>x%2 === 0)
+//start the observable using subscribe method
+//observable omits a range of numbers
+// ).subscribe(x=>console.log(x));
 
 // let nicknames = ['sam','john','james']
 // for(let nickname in nicknames){
 //   console.log(nickname)
 // }
-
-
