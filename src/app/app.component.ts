0f1c39c9498fa4765f6a6b6d2e3f821de78f98da
samndambuki <sam.ndambuki08@gmail.com>
2023-07-16 21:21:25 +0300

    passing parameters to a route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,12 @@ import { Component } from '@angular/core';
       <li><a class="nav-link" routerLink="/welcome">Home</a></li>
       <li><a class="nav-link" routerLink="/products">Product List</a></li>
     </ul>
-   </nav>`,
+   </nav>
+
+   <div class="container">
+    <router-outlet></router-outlet>
+   </div>
+   `,
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
+
+  //this component needs to read the route parameter
+  //so as to know which products details to display
+
+  //Activated Route Service provides the state of the current route, including route parameters
+
+  //set activated route as a dependency by defining it as a parameter to the constructor function
+  constructor(private route: ActivatedRoute) {}
+
+  //called when the component is initialized
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pageTitle += ` : ${id}`
+  }
+
+
 }
